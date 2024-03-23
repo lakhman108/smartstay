b0e848325f224a404f0f04d32478dfb0cbbd622a
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-03-23 14:15:28 +0530

    database configration

--- src/main/java/com/lucky/smartstay/Controller.java
+++ src/main/java/com/lucky/smartstay/Controller.java
@@ -1,5 +1,6 @@
 package com.lucky.smartstay;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
---
+++ src/main/java/com/lucky/smartstay/Controllers/PropertyController.java
@@ -0,0 +1,22 @@
+package com.lucky.smartstay.Controllers;
+
+import com.lucky.smartstay.Models.Property;
+import com.lucky.smartstay.Service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+    public class PropertyController {
+
+
+    @Autowired
+    PropertyService service;
+    @GetMapping("display")
+public List<Property> listall(){
+    return service.dispaly();
+}
+
+}
