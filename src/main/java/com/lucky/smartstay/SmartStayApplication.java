6e62c40054e0681af47bb26680a2b47465c33b8d
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-03-23 14:15:28 +0530

    database configration

---
+++ src/main/java/com/lucky/smartstay/SmartStayApplication.java
@@ -0,0 +1,14 @@
+package com.lucky.smartstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStayApplication.class, args);
+		System.out.println("SmartStay Application Start!");
+	}
+
+}
---
+++ src/test/java/com/lucky/smartstay/SmartStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.smartstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
